9f5bf7e82a7db8bffb9359ba51bf5c7b162da646
ambrouse <thichthichich3568@gmail.com>
2025-12-05 17:22:37 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/root-project-react-deploy-github/',
+  base: '/note-online-frontend/',
 })
